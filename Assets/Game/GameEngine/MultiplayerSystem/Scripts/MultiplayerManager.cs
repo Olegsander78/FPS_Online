2672eff9722174951af2bc65d3681356d0da6f8d
Oleg K <onkotov@gmail.com>
2023-07-25 17:13:26 +0500

    Добавил в MultiplayerManager код для врага

--- Assets/Game/GameEngine/MultiplayerSystem/Scripts/MultiplayerManager.cs
+++ Assets/Game/GameEngine/MultiplayerSystem/Scripts/MultiplayerManager.cs
@@ -1,11 +1,25 @@
 using Colyseus;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class MultiplayerManager : ColyseusManager<MultiplayerManager>
 {
+    [SerializeField]
+    private GameObject _player;
+
+    [SerializeField]
+    private Transform _parent;
+
+    [SerializeField]
+    private GameObject _enemy;
+
+    [SerializeField]
+    private Transform _parentEnemy;
+
     private ColyseusRoom<State> _room;
+
     protected override void Awake()
     {
         base.Awake();
@@ -17,6 +31,31 @@ public class MultiplayerManager : ColyseusManager<MultiplayerManager>
     private async void Connect()
     {
         _room = await Instance.client.JoinOrCreate<State>("state_handler");
+
+        _room.OnStateChange += OnChange;
+    }
+
+    private void OnChange(State state, bool isFirstState)
+    {
+        if (!isFirstState)
+            return;
+
+        var player = state.players[_room.SessionId];
+        var position = new Vector3(player.x - 200, 0f, player.y - 200) / 8;
+
+        Instantiate(_player, position, Quaternion.identity, _parent);
+
+        state.players.ForEach(ForEachEnemy);
+    }
+
+    private void ForEachEnemy(string key, Player player)
+    {
+        if (key == _room.SerializerId)
+            return;
+
+        var position = new Vector3(player.x - 200, 0f, player.y - 200) / 8;
+
+        Instantiate(_enemy, position, Quaternion.identity, _parentEnemy);
     }
 
     protected override void OnDestroy()
