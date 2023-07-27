21200a50440743bdcec38f1f197cee23c0aa6340
Oleg K <onkotov@gmail.com>
2023-07-27 17:06:15 +0500

    Сделал интерполяцию через Lerp
    Есть небольшая инерция

--- Assets/Game/Gameplay/Enemies/Scripts/Controllers/EnemyMoveController.cs
+++ Assets/Game/Gameplay/Enemies/Scripts/Controllers/EnemyMoveController.cs
@@ -4,63 +4,61 @@ using UnityEngine;
 
 public class EnemyMoveController : MonoBehaviour
 {
+    private Vector3 _lastPosition;
+    private Vector3 _velocity;
+
+    [SerializeField]
+    private float _interpolationTime = 0.1f;
+        
+    private float _interpolationTimer = 0f;
+
+    private Vector3 _smoothVelocity;
+
+    private void Start()
+    {
+        _lastPosition = transform.position;
+    }
+
+
+    //private void Update()
+    //{
+    //    transform.position = Vector3.SmoothDamp(transform.position, _lastPosition , ref _smoothVelocity, 0.1f);
+    //}
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, _lastPosition + _velocity, Time.deltaTime);
+    }
+
     internal void OnChange(List<DataChange> changes)
     {
-        var position = transform.position;
-        var velocity = new Vector3();
+        if (changes == null)
+            return;
 
         foreach (var dataChange in changes)
         {
             switch (dataChange.Field)
             {
                 case "x":
-                    position.x = (float)dataChange.Value;
+                    _velocity.x = (float)dataChange.Value - _lastPosition.x;
+                    _lastPosition.x = (float)dataChange.Value;
                     break;
                 case "y":
-                    position.z = (float)dataChange.Value;
+                    _velocity.z = (float)dataChange.Value - _lastPosition.z; 
+                    _lastPosition.z = (float)dataChange.Value;
                     break;
                 case "vx":
-                    velocity.x = (float)dataChange.Value;
+                    _velocity.x = (float)dataChange.Value;
                     break;
                 case "vy":
-                    velocity.z = (float)dataChange.Value;
+                    _velocity.z = (float)dataChange.Value;
                     break;
                 default:
                     Debug.LogWarning("Не обрабатывается изменение поля: " + dataChange.Field);
                     break;
             }
         }
-
-        transform.position = position;
+       
+        _interpolationTimer = _interpolationTime;
     }
 }
-
-//using Colyseus.Schema;
-//using System.Collections.Generic;
-//using UnityEngine;
-
-//public class EnemyMoveController : MonoBehaviour
-//{
-//    internal void OnChange(List<DataChange> changes)
-//    {
-//        var position = transform.position;
-
-//        foreach (var dataChange in changes)
-//        {
-//            switch (dataChange.Field)
-//            {
-//                case "x":
-//                    position.x = (float)dataChange.Value;
-//                    break;
-//                case "y":
-//                    position.z = (float)dataChange.Value;
-//                    break;
-//                default:
-//                    Debug.LogWarning("Не обрабатывается изменение поля: " + dataChange.Field);
-//                    break;
-//            }
-//        }
-
-//        transform.position = position;
-//    }
-//}
--- Assets/Game/Gameplay/Player/Scripts/PlayerCharacter.cs
+++ Assets/Game/Gameplay/Player/Scripts/PlayerCharacter.cs
@@ -2,13 +2,22 @@ using UnityEngine;
 
 public class PlayerCharacter : MonoBehaviour
 {
+    public float Speed
+    {
+        get { return _speed; }
+        set { _speed = value; }
+    }
+    
     [SerializeField]
-    private float _speed = 2f;
+    private float _speed = 3f;
+
+    [SerializeField]
+    private float _interpoliationTime = 0.8f;
 
     private float _inputH, _inputV;
     private Vector3 _lastPosition;
+    private Vector3 _predictedPosition;
     private Vector3 _velocity;
-
     public void SetInput(float h, float v)
     {
         _inputH = h;
@@ -21,6 +30,11 @@ public class PlayerCharacter : MonoBehaviour
         velocity = _velocity;
     }
 
+    private void Start()
+    {
+        _lastPosition = transform.position;
+        _predictedPosition = transform.position;
+    }
     private void Update()
     {
         Move();
@@ -29,78 +43,41 @@ public class PlayerCharacter : MonoBehaviour
     private void Move()
     {
         var direction = new Vector3(_inputH, 0f, _inputV).normalized;
-        transform.Translate(direction * Time.deltaTime * _speed, Space.World);
+        transform.position += direction * Time.deltaTime * _speed;
 
-        _velocity = (transform.position - _lastPosition) / Time.deltaTime;
+        var displacement = transform.position - _lastPosition;
+        var displacementMagnitude = displacement.magnitude;
+
+        if (displacementMagnitude > 0f)
+        {
+            _velocity = displacement / Time.deltaTime;
+        }
+        else
+        {
+            _velocity = Vector3.zero;
+        }
+
         _lastPosition = transform.position;
+
+        _predictedPosition = transform.position + _velocity * Time.deltaTime;
+
+        if (_predictedPosition != _lastPosition)
+        {
+            transform.position = Vector3.Lerp(transform.position, _predictedPosition, _interpoliationTime);
+        }
     }
+
+    //private void Move()
+    //{
+    //    var direction = new Vector3(_inputH, 0f, _inputV).normalized;
+    //    transform.position += direction * Time.deltaTime * _speed;
+
+    //    _velocity = (transform.position - _lastPosition) / Time.deltaTime;
+    //    _lastPosition = transform.position;
+
+    //    _predictedPosition = transform.position + _velocity * Time.deltaTime;
+
+    //    transform.position = Vector3.Lerp(transform.position, _predictedPosition, _interpoliationTime);
+    //}
 }
 
-//public class PlayerCharacter : MonoBehaviour
-//{
-//    [SerializeField]
-//    private float _speed = 2f;
-
-//    private float _inputH, _inputV;
-//    private Vector3 _lastPosition;
-//    private Vector3 _velocity;
-
-//    public void SetInput(float h, float v)
-//    {
-//        _inputH = h;
-//        _inputV = v;
-//    }
-
-//    public void GetMoveInfo(out Vector3 position, out Vector3 velocity)
-//    {
-//        position = transform.position;
-//        velocity = _velocity;
-//    }
-//    public Vector3 GetPredictedPosition(float time)
-//    {
-//        var direction = new Vector3(_inputH, 0f, _inputV).normalized;
-//        return transform.position + direction * _speed * time;
-//    }
-//    private void Update()
-//    {
-//        Move();
-//    }
-
-//    private void Move()
-//    {
-//        var direction = new Vector3(_inputH, 0f, _inputV).normalized;
-//        transform.position += direction * Time.deltaTime * _speed;
-
-//        _velocity = (transform.position - _lastPosition) / Time.deltaTime;
-//        _lastPosition = transform.position;
-//    }
-//}
-
-//public class PlayerCharacter : MonoBehaviour
-//{
-//    [SerializeField]
-//    private float _speed = 2f;
-
-//    private float _inputH, _inputV;
-
-//    public void SetInput(float h, float v)
-//    {
-//        _inputH = h;
-//        _inputV = v;
-//    }
-//    public void GetMoveInfo(out Vector3 position)
-//    {
-//        position = transform.position;
-//    }
-
-//    private void Update()
-//    {
-//        Move();
-//    }
-
-//    private void Move()
-//    {
-//        var direction = new Vector3(_inputH, 0f, _inputV).normalized;
-//        transform.position += direction * Time.deltaTime * _speed;
-//    }
-//}
